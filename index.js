5ad1e6bb428b3f31c9f0af44bd494c77cd97cf59
Glitch (greet-bot) <none>
2023-10-17 13:42:19 +0000

    🔮🚟 Checkpoint ./index.js:62189162/932

--- index.js
+++ index.js
@@ -41,11 +41,64 @@
 
 
 
+// const { App } = require('@slack/bolt');
+
+// const app = new App({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+//   socketMode: true,
+//   appToken: process.env.SLACK_APP_TOKEN,
+//   // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
+//   // 何らかのポートをリッスンする必要があります
+//   port: process.env.PORT || 3000
+// });
+
+// // "hello" を含むメッセージをリッスンします
+// app.message('hello', async ({ message, say }) => {
+//   // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+//   await say({
+//     blocks: [
+//       {
+//         "type": "section",
+//         "text": {
+//           "type": "mrkdwn",
+//           "text": `Hey there <@${message.user}>!`
+//         },
+//         "accessory": {
+//           "type": "button",
+//           "text": {
+//             "type": "plain_text",
+//             "text": "Click Me"
+//           },
+//           "action_id": "button_click"
+//         }
+//       }
+//     ],
+//     text: `Hey there <@${message.user}>!`
+//   });
+// });
+
+// app.action('button_click', async ({ body, ack, say }) => {
+//   // アクションのリクエストを確認
+//   await ack();
+//   await say(`<@${body.user.id}> clicked the button`);
+// });
+
+// (async () => {
+//   // アプリを起動します
+//   await app.start();
+
+//   console.log('⚡️ Bolt app is running!');
+// })();
+
+
+
+
 const { App } = require('@slack/bolt');
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
   // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
@@ -56,32 +109,7 @@ const app = new App({
 // "hello" を含むメッセージをリッスンします
 app.message('hello', async ({ message, say }) => {
   // イベントがトリガーされたチャンネルに say() でメッセージを送信します
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // アクションのリクエストを確認
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
+  await say(`Hey there <@${message.user}>!`);
 });
 
 (async () => {
