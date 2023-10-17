1e2f9c06b91d3e57970f8e5f4a1c1790259a3c5e
Glitch (greet-bot) <none>
2023-10-17 13:54:13 +0000

    🎾💏 Checkpoint ./index.js:62189162/552

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-// const { App } = require('@slack/bolt');
-// const store = require('./store');
+const { App } = require('@slack/bolt');
+const store = require('./store');
 
-// const app = new App({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   token: process.env.SLACK_BOT_TOKEN,
-//   // socketMode: true,
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
 
-// });
-
-
-// app.event('app_mention', async ({ event, say }) => {
-//   // Look up the user from DB
-//   let user = store.getUser(event.user);
-//   // if (user) {
-//     user = {
-//       user: event.user,
-//       channel: event.channel,
-//       // count: 1
-//     };
-//     store.addUser(user);
-//     await say(`Hello world, and welcome <@${event.user}>`);
-//   // } else {
-//   //   await say('Hi again!');
-//   // }
-// });
-
-// app.message('もくもく',async({message,say})=>{
-//   await say(`もくもくっていったね`);
-// });
+});
 
 
-// // Start your app
-// (async () => {
-//   await app.start(process.env.PORT || 3000);
-//   console.log('⚡️ Bolt app is running!');
-// })();
+app.event('app_mention', async ({ event, say }) => {
+  // Look up the user from DB
+  let user = store.getUser(event.user);
+  // if (user) {
+    user = {
+      user: event.user,
+      channel: event.channel,
+      // count: 1
+    };
+    store.addUser(user);
+    await say(`Hello world, and welcome <@${event.user}><${event.ts}>`);
+  // } else {
+  //   await say('Hi again!');
+  // }
+});
+
+app.message('もくもく',async({message,say})=>{
+  await say(`もくもくっていったね`);
+});
+
+
+// Start your app
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
 
 
 
@@ -94,27 +93,44 @@
 
 
 
-const { App } = require('@slack/bolt');
+// const { App } = require('@slack/bolt');
 
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
-  // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
-  // 何らかのポートをリッスンする必要があります
-  port: process.env.PORT || 3000
-});
+// const app = new App({
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+//   token: process.env.SLACK_BOT_TOKEN,
+//   socketMode: true,
+//   appToken: process.env.SLACK_APP_TOKEN,
+//   // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
+//   // 何らかのポートをリッスンする必要があります
+//   port: process.env.PORT || 3000
+// });
 
-// "hello" を含むメッセージをリッスンします
-app.message('hello', async ({ message, say }) => {
-  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
-  await say(`Hey there <@${message.user}>!`);
-});
+// // "hello" を含むメッセージをリッスンします
+// app.message('hello', async ({ message, say }) => {
+//   // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+//   await say(`Hey there <@${message.user}>!`);
+// });
 
-(async () => {
-  // アプリを起動します
-  await app.start();
+// (async () => {
+//   // アプリを起動します
+//   await app.start();
 
-  console.log('⚡️ Bolt app is running!');
-})();
+//   console.log('⚡️ Bolt app is running!');
+// })();
+
+
+
+// const { App } = require('@slack/bolt');
+
+// // ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+// const app = new App({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET
+// });
+
+// (async () => {
+//   // アプリを起動します
+//   await app.start(process.env.PORT || 3000);
+
+//   console.log('⚡️ Bolt app is running!');
+// })();
