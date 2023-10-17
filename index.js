e6fdf19d04b8c197db61158f32a611596b09dc13
Glitch (greet-bot) <none>
2023-10-17 13:31:01 +0000

    🏢🛩 Checkpoint ./index.js:62189162/1561

--- index.js
+++ index.js
@@ -1,32 +1,92 @@
+// const { App } = require('@slack/bolt');
+// const store = require('./store');
+
+// const app = new App({
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+//   token: process.env.SLACK_BOT_TOKEN,
+//   // socketMode: true,
+
+// });
+
+
+// app.event('app_mention', async ({ event, say }) => {
+//   // Look up the user from DB
+//   let user = store.getUser(event.user);
+//   // if (user) {
+//     user = {
+//       user: event.user,
+//       channel: event.channel,
+//       // count: 1
+//     };
+//     store.addUser(user);
+//     await say(`Hello world, and welcome <@${event.user}>`);
+//   // } else {
+//   //   await say('Hi again!');
+//   // }
+// });
+
+// app.message('もくもく',async({message,say})=>{
+//   await say(`もくもくっていったね`);
+// });
+
+
+// // Start your app
+// (async () => {
+//   await app.start(process.env.PORT || 3000);
+//   console.log('⚡️ Bolt app is running!');
+// })();
+
+
+
+
+
+
 const { App } = require('@slack/bolt');
-const store = require('./store');
 
 const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
+  // 何らかのポートをリッスンする必要があります
+  port: process.env.PORT || 3000
 });
 
-
-app.event('app_mention', async ({ event, say }) => {
-  // Look up the user from DB
-  let user = store.getUser(event.user);
-  // if (user) {
-    user = {
-      user: event.user,
-      channel: event.channel,
-      // count: 1
-    };
-    store.addUser(user);
-    await say(`Hello world, and welcome <@${event.user}><${event.channel}>`);
-  // } else {
-  //   await say('Hi again!');
-  // }
+// "hello" を含むメッセージをリッスンします
+app.message('hello', async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
 });
 
+app.action('button_click', async ({ body, ack, say }) => {
+  // アクションのリクエストを確認
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
 
-// Start your app
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  // アプリを起動します
+  await app.start();
+
   console.log('⚡️ Bolt app is running!');
 })();
-
