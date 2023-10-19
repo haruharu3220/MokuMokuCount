93e2db94ae015d948d04137539fb253717efee35
Glitch (greet-bot) <none>
2023-10-19 09:27:04 +0000

    🌅⏲ Checkpoint ./index.js:62189162/69

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const store = require('./store');
 let userCount = {};
 var date = new Date() ;
 let sayFlag = true;
-
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
